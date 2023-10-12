316563f39a5286fabe6ab61676a256a5fa68f56c
glassofcoke <contatocostamarcos@gmail.com>
2023-10-12 12:06:29 -0300

    game quase completo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () =>{
     mario.classList.add('jump');
@@ -10,6 +11,25 @@ const jump = () =>{
     },500);
 }
 
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+       mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = '../images/game-over.png';
+        mario.sytle.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
+    }
+},10)
 
 
 
